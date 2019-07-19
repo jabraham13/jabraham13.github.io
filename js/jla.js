5614fce88bbf65df8e16cf7465c458986d21af02
julieabraham <julieabraham@localhost>
2019-07-19 16:46:59 -0400

    Adding new files from development directory. Removing unused older files.

---
+++ js/jla.js
@@ -0,0 +1,11 @@
+/* 
+    Author     : Julie Abraham
+*/
+
+function loadIndexPage() {
+    $("#faq-button").click(function(){
+//        $("#main").append("<p>Appended new HTML!<p>");
+//        $("#main").load("Some brand new text");
+        $("#main").load("faq.html");
+    });
+}
--- js/witd.js
+++
@@ -1,8 +0,0 @@
-
-function loadIndexPage() {
-    $("#home-button").click(function(){$("#main").load("home.html");});
-    $("#about-button").click(function(){$("#main").load("about.html");});
-    $("#work-button").click(function(){$("#main").load("work.html");});
-    $("#fun-button").click(function(){$("#main").load("fun.html");});
-    $("#code-button").click(function(){$("#main").load("code.html");});
-}
