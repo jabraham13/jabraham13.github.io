2aab9bc8d6c4831bacaf156e07505b0ed045eba1
julieabraham <julieabraham@localhost>
2019-07-19 18:20:36 -0400

    Bare bones version almost ready to go.

--- js/jla.js
+++ js/jla.js
@@ -4,9 +4,13 @@
 
 function loadIndexPage() {
     $("#about").click(function(){
+        $(".active").removeClass("active");
+        $("#about").addClass("active");
         $("#main").load("about.html");
     });
     $("#contact").click(function(){
+        $(".active").removeClass("active");
+        $("#contact").addClass("active");
         $("#main").load("contact.html");
     });
 }
