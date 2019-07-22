6c1ff68e2bcdb04c4820bbc943725ccf994729f3
julieabraham <julieabraham@localhost>
2019-07-22 16:49:15 -0400

    Added CV page. Temporarily removed box-shadow until I can fix scrolling.

--- js/jla.js
+++ js/jla.js
@@ -8,7 +8,12 @@ function loadIndexPage() {
         $("#about").addClass("active");
         $("#main").load("about.html");
     });
-    $("#contact").click(function(){
+    $("#cv").click(function(){
+        $(".active").removeClass("active");
+        $("#cv").addClass("active");
+        $("#main").load("cv.html");
+    });
+        $("#contact").click(function(){
         $(".active").removeClass("active");
         $("#contact").addClass("active");
         $("#main").load("contact.html");
