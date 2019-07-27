4d5e2f5f7d1768d6f0b09e326f11e6ab44f940ad
julieabraham <julieabraham@localhost>
2019-07-26 22:10:40 -0400

    Working on example css styling page.

--- js/jla.js
+++ js/jla.js
@@ -25,12 +25,24 @@ function loadPortfolioPage() {
         var workLinks = new Array();
         workLinks[0] = "style.html";
         workLinks[1] = "cpp.html";
-        window.location = workLinks[Math.floor(Math.random() * 2)]
+        window.location = workLinks[Math.floor(Math.random() * 2)];
     });
     $("#play").click(function () {
         var playLinks = new Array();
         playLinks[0] = "avatar.html";
         playLinks[1] = "pictures.html";
-        window.location = playLinks[Math.floor(Math.random() * 2)]
+        window.location = playLinks[Math.floor(Math.random() * 2)];
+    });
+}
+
+function loadStylePage() {
+    $("#btn-load-book").click(function () {
+        $("#book").load("files/205-h.html");
+    });
+    $("#btn-change-style").click(function () {
+        $("#book").css({
+            'color': 'black',
+            'font-family' : 'serif',
+            'font-size': '100%'});
     });
 }
